7a91024e18d9c55854fc5b0126620dfa98b93d66
Your Name <you@example.com>
2017-11-08 21:21:08 +0300

    init

---
+++ testProject1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace testProject1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(758, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(735, 481);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MyMouseMove);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(236, 556);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Заливка";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(236, 533);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Кисть";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 527);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Выбрать изображение...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 557);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 592);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ testProject1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testProject1
+{
+    public struct BorderPoint
+    {
+        public Point point;
+        public int preDir;
+
+        public BorderPoint(int x, int y, int pd)
+        {
+            point = new Point(x, y);
+            preDir = pd;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int sizePen;
+        Bitmap bitmap;
+        Bitmap myPicture;
+        Point mouse;
+        private static Image sampleImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(this.MyMouseMove);
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+            pictureBox1.Image = bitmap;
+
+            sizePen = 5;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = e.Location;
+        }
+
+        private void Draw(MouseEventArgs e)
+        {
+            g.DrawLine(new Pen(colorDialog1.Color, sizePen), mouse, e.Location);
+            mouse = e.Location;
+            pictureBox1.Refresh();
+        }
+
+        private void MyMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!radioButton3.Enabled || !radioButton3.Checked)
+                return;
+            if (e.Button == MouseButtons.Left)
+                Draw(e);
+        }
+
+        private void FillPicture(Point e, int x, int y, Color backColor)
+        {
+            int x1 = e.X, x2 = e.X;
+            int y1 = e.Y, y2 = e.Y;
+            Color pixelColor = new Color();
+            pixelColor = bitmap.GetPixel(x1, y1);
+
+            if (pixelColor.ToArgb() != backColor.ToArgb())
+                return;
+            // если цвет пиксела равен цвету фона
+            while (pixelColor.ToArgb() == backColor.ToArgb() && x1 > 0)
+            {
+                // цвет левого пикселя
+                --x1;
+                pixelColor = bitmap.GetPixel(x1, y1);
+            }
+            pixelColor = backColor;
+            while (pixelColor.ToArgb() == backColor.ToArgb() && x2 < pictureBox1.Image.Width - 1)
+            {
+                // цвет правого пикселя
+                ++x2;
+                pixelColor = bitmap.GetPixel(x2, y2);
+            }
+
+            // рисуем линию
+            int newX;
+            newX = myPicture.Width - (mouse.X - x1) % myPicture.Width;
+
+            if (y == -1)
+                y = myPicture.Height - 1;
+            ++x1;
+            --x2;
+
+            for (int i = x1; i < x2; ++i)
+                bitmap.SetPixel(i, y1, myPicture.GetPixel((myPicture.Width + newX++) % myPicture.Width, (myPicture.Height + y) % myPicture.Height));
+
+            for (int i = x1; i < x2; ++i)
+            {
+                if (y1 < pictureBox1.Image.Height - 1)
+                    FillPicture(new Point(i, y1 + 1), mouse.X, y + 1, backColor);
+                if (y1 > 0)
+                    FillPicture(new Point(i, y2 - 1), mouse.X, y - 1, backColor);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked && radioButton1.Enabled)
+            {
+                if (sampleImage == null)
+                {
+                    MessageBox.Show("Не установлен шаблон.");
+                    return;
+                }
+
+                FillPicture(new Point(e.X, e.Y), int.MaxValue, 0, bitmap.GetPixel(e.X, e.Y));
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!radioButton3.Checked)
+            {
+                pictureBox1.MouseMove -= MyMouseMove;
+            }
+            else
+            {
+                pictureBox1.MouseMove += MyMouseMove;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                myPicture = new Bitmap(ofd.FileName);
+
+            sampleImage = Image.FromFile(ofd.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Refresh();
+        }
+    }
+}
